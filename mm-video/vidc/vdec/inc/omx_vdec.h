6ee50881782f07feb293c7892615fc6d110677e8
Srinu Gorle <sgorle@codeaurora.org>
2013-05-13 19:47:37 +0530

    mm-video: Add data corrupt flag on output buffers when required
    - If non-IDR frames received after seek operation, corrupted YUV frames
      are displayed.
    - Add OMX_BUFFERFLAG_DATACORRUPT flag to each output buffer till
      OMX_BUFFERFLAG_SYNCFRAME received.
    - IL client can skip rendering, if DATACORRUPT flag observed.
    
    Change-Id: I9bad6635397a03a55fd602740cc520cb807cf701
    CRs-Fixed: 472798

--- mm-video/vidc/vdec/inc/omx_vdec.h
+++ mm-video/vidc/vdec/inc/omx_vdec.h
@@ -733,6 +733,7 @@ private:
     OMX_BOOL m_out_bPopulated;
     // encapsulate the waiting states.
     unsigned int m_flags;
+    OMX_BOOL m_out_sync_frm_received;
 
 #ifdef _ANDROID_
     // Heap pointer to frame buffers
