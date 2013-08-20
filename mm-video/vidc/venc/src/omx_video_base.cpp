f98508e70cbf473f7b22b0ad0eff0989ee9b8918
Rudolf Tammekivi <rtammekivi@gmail.com>
2013-08-20 10:59:47 +0300

    mm-video: Fix compile error on 7x30.
    Change-Id: Ia5d25165602eb2ae42ea8ac33248c627b07fed1d

--- mm-video/vidc/venc/src/omx_video_base.cpp
+++ mm-video/vidc/venc/src/omx_video_base.cpp
@@ -4440,6 +4440,7 @@ OMX_ERRORTYPE omx_video::get_supported_profile_level(OMX_VIDEO_PARAM_PROFILELEVE
                     profileLevelType->eProfile,profileLevelType->eLevel);
   return eRet;
 }
+#endif
 
 #ifdef USE_ION
 int omx_video::alloc_map_ion_memory(int size,struct ion_allocation_data *alloc_data,
@@ -4533,7 +4534,6 @@ void omx_video::free_ion_memory(struct venc_ion *buf_ion_info)
      buf_ion_info->fd_ion_data.fd = -1;
      pthread_mutex_unlock(&m_venc_ionlock);
 }
-#endif
 #endif
 #ifdef _ANDROID_ICS_
 void omx_video::omx_release_meta_buffer(OMX_BUFFERHEADERTYPE *buffer)
