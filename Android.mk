5d835b4901981cd2df5eb0ded624c922834cf436
RonGokhale <cips173@gmail.com>
2014-07-29 13:08:27 +0530

    media-caf: Fix building for msm7x27a
    Change-Id: I5fbbf4f955b36c14bdb960a6e02eeab7d76ec451

--- Android.mk
+++ Android.mk
@@ -1,8 +1,14 @@
-ifneq ($(filter msm8960 msm8660 msm7x30,$(TARGET_BOARD_PLATFORM)),)
 ifeq ($(TARGET_QCOM_MEDIA_VARIANT),caf)
+QCOM_MEDIA_ROOT := $(call my-dir)
+ifneq ($(filter msm8960 msm8660 msm7x30 msm7x27a,$(TARGET_BOARD_PLATFORM)),)
+include $(QCOM_MEDIA_ROOT)/mm-core/Android.mk
+include $(QCOM_MEDIA_ROOT)/libstagefrighthw/Android.mk
+include $(QCOM_MEDIA_ROOT)/dashplayer/Android.mk
+include $(QCOM_MEDIA_ROOT)/QCMediaPlayer/Android.mk
+endif
 
-include $(call all-subdir-makefiles)
-
+ifneq ($(filter msm8960 msm8660 msm7x30,$(TARGET_BOARD_PLATFORM)),)
+include $(QCOM_MEDIA_ROOT)/mm-video/Android.mk
+include $(QCOM_MEDIA_ROOT)/libc2dcolorconvert/Android.mk
 endif
 endif
-
